f3c843e97e88bb990bcbf1f18e1cfe406598bc25
julhanlon <jhanlon289@gmail.com>
2020-07-13 08:12:07 -0700

    added email functions and images

--- models/plants.js
+++ models/plants.js
@@ -1,5 +1,9 @@
 module.exports = (sequelize, DataTypes) => {
     const Plants = sequelize.define("Plant", {
+        common_name:{
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
         water:{
             type: DataTypes.INTEGER,
             allowNull: false,
