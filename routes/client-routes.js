c06447cedbf9847b9d1a41c111cbce6f217e7b69
Michelle Tran <mptran0101@berkeley.edu>
2020-07-13 20:17:12 -0700

    added additional routes

--- routes/client-routes.js
+++ routes/client-routes.js
@@ -6,10 +6,6 @@ const axios = require("axios");
 
 require("dotenv").config();
 
-const express = require("express");
-const router = express.Router();
-const path = require("path");
-
 router.get("/", (req, res) =>
   !req.user
     ? res.sendFile(path.join(__dirname, "../client/landing.html"))
