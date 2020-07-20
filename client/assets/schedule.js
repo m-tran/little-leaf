72d1ec3a55cb13a099d4cc3a726422a48f830bec
julhanlon <jhanlon289@gmail.com>
2020-07-19 21:40:31 -0700

    scheduler updated

--- client/assets/schedule.js
+++ client/assets/schedule.js
@@ -11,5 +11,34 @@ $("#day4").text(moment().add(3, 'days').format('L'));
 $("#day5").text(moment().add(4, 'days').format('L'));
 $("#day6").text(moment().add(5, 'days').format('L'));
 $("#day7").text(moment().add(6, 'days').format('L'));
+
+
+
+
+const getPlants = () => {
+    return new Plants((resolve, reject) => {
+        $.ajax({
+            type: "GET",
+            url: "/plants/all",
+        })
+        .then((res) => {
+            resolve(res);
+        })
+        .catch((err) => reject(err));
+    });
+};
+
+getPlants().then((res) => {
+    console.log(res)
+    res.forEach((plant) => {
+    res.send(plant.water_frequency, plant.prune_frequency, plant.rotate_frequency, plant.rotate_frequency)
+
+    });
 });
 
+
+
+
+
+
+});
